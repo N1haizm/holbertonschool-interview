7d3c06e05b110ca514065774286e43ef4b8752fa
N1haizm <105300243+N1haizm@users.noreply.github.com>
2024-02-29 23:32:50 +0400

    Update 0-lockboxes.py

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
-"""I wrote this code to solve the boxes problem"""
+"""Solves the boxes problem."""
 
-def canUnlockAll(boxes):
+
+def can_unlock_all(boxes):
+    """Checks if all boxes can be unlocked.
+
+    Args:
+        boxes: A list of lists, where each inner list represents the keys held
+               by a box.
+
+    Returns:
+        True if all boxes can be unlocked, False otherwise.
+    """
+
     for key in range(1, len(boxes)):
-        flag = False
-        for box in range(len(boxes)):
-            if key in boxes[box] and box != key:
-                flag = True
+        can_be_unlocked = False
+        for box_index in range(len(boxes)):
+            if key in boxes[box_index] and box_index != key:
+                can_be_unlocked = True
                 break
-        if not flag:
+        if not can_be_unlocked:
             return False
     return True
